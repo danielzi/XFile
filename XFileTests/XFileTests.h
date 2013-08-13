82d2a2239980f5224e6727ca47b88b89a94e7d7c
liaosiyuan <liaosiyuan@sharera.com>
2013-08-13 10:25:59 +0800

    init

---
+++ XFileTests/XFileTests.h
@@ -0,0 +1,13 @@
+//
+//  XFileTests.h
+//  XFileTests
+//
+//  Created by liaosiyuan on 13-8-13.
+//  Copyright (c) 2013年 liaosiyuan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XFileTests : SenTestCase
+
+@end
